2b50146da294d823001e5d50d3549d021e282a42
Gabriel Rodrigues <gabrielcr18@outlook.com>
2022-03-29 17:42:14 -0300

    revisão inicial do codigo

--- tabuleiro.c
+++ tabuleiro.c
@@ -47,10 +47,9 @@ void alocaNomeArquivo(char **nomeArquivo, int qtd){
 
 void desalocaNomeArquivo(char **nomeArquivo){
   free(*nomeArquivo);
-
 }
 
-void alocaCelula(QCelula **Celula){
+/*void alocaCelula(QCelula **Celula){
   *Celula = (QCelula*) malloc(sizeof(QCelula) * 9);
 
   if (*Celula == NULL ) {
@@ -62,6 +61,7 @@ void alocaCelula(QCelula **Celula){
 void desalocacelula(QCelula **Celula){
   free(*Celula);
 }
+*/
 
 void TabuleiroInicializa(char *nomeArquivo, QTabuleiro* Tabuleiro){
 
@@ -82,11 +82,7 @@ void TabuleiroInicializa(char *nomeArquivo, QTabuleiro* Tabuleiro){
       fscanf(arquivo, "%i", &valor);
       fscanf(arquivo, "%c", &tmp);
       (*Tabuleiro).celulas[i][j].conteudo = valor;
-      (*Tabuleiro).celulas[i][j].estado[0] = 0;
-      (*Tabuleiro).celulas[i][j].estado[1] = 0;
-      (*Tabuleiro).celulas[i][j].estado[2] = 0;
-      (*Tabuleiro).celulas[i][j].estado[3] = 0;
-
+      
       //printf("%i ", (*Tabuleiro).celulas[i][j].conteudo);
     }
     //printf("\n");    
@@ -97,98 +93,14 @@ void TabuleiroInicializa(char *nomeArquivo, QTabuleiro* Tabuleiro){
 int defineVazias(QTabuleiro* Tabuleiro, int lin, int col){
 
   if((*Tabuleiro).celulas[lin][col].conteudo == 0){
-    (*Tabuleiro).celulas[lin][col].estado[0] = -1;
+   // (*Tabuleiro).celulas[lin][col].estado[0] = -1;
     return 1;
   }
   return 0;
 }
 
+
+
 int EhValido(QTabuleiro* Tabuleiro, int lin, int col, int metodo, QCelula* Conflitos){
-
-  int k = 1, entrou = 0, retorno = 1;
-
-  //Verifica Linha
-  if(metodo == 0 && (*Tabuleiro).celulas[lin][col].estado[0] != 1){
-    for (int j = 0; j < 9; j++){
-      if(j != col && (*Tabuleiro).celulas[lin][col].conteudo == (*Tabuleiro).celulas[lin][j].conteudo && (*Tabuleiro).celulas[lin][j].estado[1] == 0){
-        (Conflitos[k]).lin = lin;
-        (Conflitos[k]).col = j;
-        k++;
-
-        (*Tabuleiro).celulas[lin][j].estado[0] = 1;
-        (*Tabuleiro).celulas[lin][j].estado[1] = 1;
-
-        entrou = 1;
-      }
-    }
-
-    if(entrou == 1){
-      printf("\nLinha %i: ", lin+1);
-
-      (Conflitos[0]).lin = lin;
-      (Conflitos[0]).col = col;
-
-      (*Tabuleiro).celulas[lin][col].estado[0] = 1;
-      (*Tabuleiro).celulas[lin][col].estado[1] = 1;
-
-      retorno = 0;
-    }
-
-    (Conflitos[k+1]).lin = -1;
-    (Conflitos[k+1]).col = -1;
-
-    if(k != 1){
-      for (int i = 0; i < k; i++){
-        printf("(%i, %i)", Conflitos[i].lin+1, Conflitos[i].col+1);
-        
-        if ((Conflitos[i+2]).lin != -1)
-          printf(" e ");
-      } 
-    }
-  }
   
-  //Verifica Coluna
-  if(metodo == 1 && (*Tabuleiro).celulas[lin][col].estado[0] != -1){
-    for (int j = 0; j < 9; j++){
-      if(j != col && (*Tabuleiro).celulas[lin][col].conteudo == (*Tabuleiro).celulas[lin][j].conteudo && (*Tabuleiro).celulas[lin][j].estado[1] == 0){
-        (Conflitos[k]).lin = lin;
-        (Conflitos[k]).col = j;
-        k++;
-
-        (*Tabuleiro).celulas[lin][j].estado[0] = 1;
-        (*Tabuleiro).celulas[lin][j].estado[1] = 1;
-
-        entrou = 1;
-      }
-    }
-
-    if(entrou == 1){
-      printf("\nLinha %i: ", lin+1);
-
-      (Conflitos[0]).lin = lin;
-      (Conflitos[0]).col = col;
-
-      (*Tabuleiro).celulas[lin][col].estado[0] = 1;
-      (*Tabuleiro).celulas[lin][col].estado[1] = 1;
-
-      retorno = 0;
-    }
-
-    (Conflitos[k+1]).lin = -1;
-    (Conflitos[k+1]).col = -1;
-
-    if(k != 1){
-      for (int i = 0; i < k; i++){
-        printf("(%i, %i)", Conflitos[i].lin+1, Conflitos[i].col+1);
-        
-        if ((Conflitos[i+2]).lin != -1)
-          printf(" e ");
-      } 
-    }
-  }
-
-  //Verifica Região
-
-  return retorno;
-
 }
